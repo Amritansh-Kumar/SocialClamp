a9cc47b339de8b58e6efc30032006a682b56d0cf
Amritansh-Kumar <yadavamritanshami@gmail.com>
2018-10-06 23:26:57 +0530

    added FirebasecloudMessaging

---
+++ app/src/main/java/com/example/amritansh/socialclamp/MyFirebaseMessagingService.java
@@ -0,0 +1,68 @@
+package com.example.amritansh.socialclamp;
+
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.media.RingtoneManager;
+import android.net.Uri;
+import android.support.v4.app.NotificationCompat;
+import android.support.v4.app.NotificationManagerCompat;
+import android.util.Log;
+
+import com.example.amritansh.socialclamp.activities.HomeActivity;
+import com.google.firebase.messaging.FirebaseMessagingService;
+import com.google.firebase.messaging.RemoteMessage;
+
+public class MyFirebaseMessagingService extends FirebaseMessagingService {
+
+
+
+    private static final String TAG = "FirebaseMessagingServce";
+    private static final int ONGOING_NOTIFICATION_ID = 1;
+    private static final Object CHANNEL_ID = 2;
+
+    @Override
+    public void onMessageReceived(RemoteMessage remoteMessage) {
+
+        Log.d("notification", "notificationreceived");
+
+        String notificationTitle = null, notificationBody = null;
+
+        // Check if message contains a notification payload.
+        if (remoteMessage.getNotification() != null) {
+            Log.d(TAG, "Message Notification Body: " + remoteMessage.getNotification().getBody());
+            notificationTitle = remoteMessage.getNotification().getTitle();
+            notificationBody = remoteMessage.getNotification().getBody();
+        }
+
+        // Also if you intend on generating your own notifications as a result of a received FCM
+        // message, here is where that should be initiated. See sendNotification method below.
+        sendNotification(notificationTitle, notificationBody);
+    }
+
+
+    private void sendNotification(String notificationTitle, String notificationBody) {
+        Intent intent = new Intent(this, HomeActivity.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent,
+                PendingIntent.FLAG_ONE_SHOT);
+
+        NotificationCompat.Builder mBuilder = new NotificationCompat.Builder(this, "1")
+                .setSmallIcon(R.drawable.useravtar)
+                .setContentTitle(notificationTitle)
+                .setContentText(notificationBody)
+                .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+                .setContentIntent(pendingIntent)
+                .setAutoCancel(true);
+
+        NotificationManagerCompat notificationManager = NotificationManagerCompat.from(this);
+
+        notificationManager.notify(ONGOING_NOTIFICATION_ID, mBuilder.build());
+
+    }
+
+
+
+}
